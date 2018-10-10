576a647376fb55b56ba0ecf4be86398ce1a1278f
Elleree Carlton <ellecarlton21@gmail.com>
2018-10-10 14:14:22 -0400

    first commit

--- form.php
+++ form.php
@@ -1,50 +1,59 @@
-<form name="simpleform" method="post" action="simple_form_send.php">
-    <table width="450px">
-    <tr>
-        <td valign="top">
-        <label for="first_name">First name *</label>
-    </td>
-    <td valign="top">
-        <input type="text" name="first_name" maxlength="50" size="30">
-    </td>
-    </tr>
-
-    <tr>
-        <td valign="top">
-            <label for="last_name">Last Name *</label>
-        </td>
-        <td valign="top">
-            <input type="text" name="last_name" maxlength="50" size="30">
-        </td>
-    </tr>
-    <tr>
-        <td valign="top">
-            <label for="email">Email Address *</label>
-        </td>
-        <td valign="top">
-            <input type="text" name="email" maxlength="80" size="30">
-        </td>
-    </tr>
-    <tr>
-    <td valign="top">
-        <label for="telephone">Telephone Number</label>
-    </td>
-    <td valign="top">
-        <input type="text" name="telephone" maxlength="30" size="30">
-    </td>
-    </tr>
-    <tr>
-        <td valign="top">
-            <label for="comments">Comments *</label>
-        </td>
-        <td valign="top">
-            <textarea name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2" style="text-align:center">
-            <input type="submit" value="Submit">
-        </td>
-    </tr>
-</table>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <input type="text" name="name">
+    <span class="error">* <?php echo $nameMis;?></span>
+    <br></br>
+    <input type="text" name="email">
+    <span class="error">* <?php echo $emailMis;?></span>
+    <br></br>
+    <input type="text" name="website">
+    <span class="error">*<?php echo $websiteMis;?></span>
+    <br></br>
+    <textarea name="comment" rows="4" cols="40"></textarea>
+    <br></br>
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+    <span class="error">* <?php echo $genderMis;?></span>
+    <input type="submit" name="submit" value="Submit">
 </form>
+<?php
+
+$nameMis = $emailMis = $genderMis = $websiteMis = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameMis = "Name is required";
+    }   else {
+        $name = test_input ($_POST["name"]);
+    }
+    if (empty($_POST["email"])) {
+        $emailMis = "Email is required";
+    }   else {
+        $email = test_input ($_POST["email"]);
+    }
+    if (empty($_POST["website"])) {
+        $website = "";
+    }   else {
+        $website = test_input ($_POST["website"]);
+    }
+    if (empty($_POST["comment"])) {
+        $comment = "";
+    }   else {
+        $comment = test_input ($_POST["comment"]);
+    }
+    if (empty($_POST["gender"])) {
+        $genderMis = "Gender is required";
+    }   else {
+        $gender = test_input ($_POST["gender"]);
+    }
+}
+
+function test_input ($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
